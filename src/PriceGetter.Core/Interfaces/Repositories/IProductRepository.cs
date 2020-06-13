7b21e15a1a0dc46979b3747efb936d82b16fbf12
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-06-13 22:48:10 +0200

    Created first repository

---
+++ src/PriceGetter.Core/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using PriceGetter.Core.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceGetter.Core.Interfaces.Repositories
+{
+    public interface IProductRepository : IRepository
+    {
+        Task<Product> Get(Guid productId);
+        Task Save(Product product);
+    }
+}
---
+++ src/PriceGetter.Persistence/Repositories/InMemoryProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using PriceGetter.Core.Interfaces.Repositories;
+using PriceGetter.Core.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceGetter.Persistence.Repositories
+{
+    public class InMemoryProductRepository : IProductRepository
+    {
+        private List<Product> products;
+
+        public InMemoryProductRepository()
+        {
+            this.products = new List<Product>();
+        }
+
+        public async Task<Product> Get(Guid productId)
+        {
+            return await Task.FromResult(this.products.Single(x => x.Id == productId));
+        }
+
+        public async Task Save(Product product)
+        {
+            if(this.products.Contains(product) == false)
+            {
+                this.products.Add(product);
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}

b8da765807034a31712d7b28aaa3139f76e6af1b
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-06-20 19:34:26 +0200

    First objects for product application service

---
+++ src/PriceGetter.Contracts/Products/PriceDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceGetter.Contracts.Products
+{
+    public class PriceDto
+    {
+        public Guid SellerId { get; set; }
+        public decimal Price { get; set; }
+        public DateTime At { get; set; }
+    }
+}
---
+++ src/PriceGetter.Contracts/Products/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceGetter.Contracts.Products
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<PriceDto> Prices { get; set; }   
+    }
+}

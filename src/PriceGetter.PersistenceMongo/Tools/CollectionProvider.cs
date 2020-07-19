0ccca63e68e6827a400934d903fbcf384f6f4eca
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-07-19 18:06:08 +0200

    Created collection provider for mongo collections

---
+++ src/PriceGetter.PersistenceMongo/Tools/CollectionProvider.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using PriceGetter.Infrastructure.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceGetter.PersistenceMongo.Tools
+{
+    public class CollectionProvider : ICollectionProvider
+    {
+        private readonly IMongoDatabase database;
+        private static readonly string databaseName = "PriceGetter";
+
+        public CollectionProvider(DatabaseSettings settings)
+        {
+            IMongoClient client = new MongoClient(settings.NoSqlConnectionString);
+            this.database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<T> Get<T>(string collectionName)
+        {
+            return this.database.GetCollection<T>(collectionName);
+        }
+    }
+}
---
+++ src/PriceGetter.PersistenceMongo/Tools/ICollectionProvider.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceGetter.PersistenceMongo.Tools
+{
+    public interface ICollectionProvider
+    {
+        IMongoCollection<T> Get<T>(string collectionName);
+    }
+}

df10542018a5ecca4c1c5ddcb00096fa9202c364
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-07-19 19:13:24 +0200

    Tests for mongo collections

---
+++ tests/PriceGetter.PersistenceMongoTests/Tools/CollectionsTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using PriceGetter.PersistenceMongo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace PriceGetter.PersistenceMongoTests.Tools
+{
+    public class CollectionsTests
+    {
+        [Fact]
+        public void All_ShouldNotBeEmpty()
+        {
+            Collections.All().Should().NotBeEmpty();
+        }
+
+        [Theory]
+        [InlineData("Products")]
+        [InlineData("Sellers")]
+        [InlineData("ProductFollows")]
+        public void Should_Contain_CriticalCollections(string collectionName)
+        {
+            IEnumerable<string> collections = Collections.All();
+
+            collections
+                .SingleOrDefault(x => x == collectionName)
+                .Should().NotBeNullOrEmpty();
+        }
+    }
+}

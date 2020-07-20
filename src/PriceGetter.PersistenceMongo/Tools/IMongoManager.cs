7ba6f4d05c4e1aded946691468903bdd79b66b32
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-07-20 18:00:09 +0200

    Nothing really important

---
+++ src/PriceGetter.PersistenceMongo/Tools/IMongoManager.cs
@@ -0,0 +1,18 @@
+﻿using PriceGetter.Infrastructure.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceGetter.PersistenceMongo.Tools
+{
+    public interface IMongoManager // to be created in integration tests module
+    {
+        bool DoesDatabaseExist(MongoSettings dbSettings);
+        bool CreateDatabase(MongoSettings dbSettings);
+        bool RemoveDatabase(MongoSettings dbSettings);
+
+        bool DoesCollectionExists(string collectionName);
+        bool CreateCollection(string collectionName);
+        bool RemoveCollection(string collectionName);
+    }
+}

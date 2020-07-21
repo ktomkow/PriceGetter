90aebb602f38dae697e5338c1db32ab8b28e9720
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-07-21 14:47:28 +0200

    Created first method in product service

--- src/PriceGetter.ApplicationService/ProductServices/IProductService.cs
+++ src/PriceGetter.ApplicationService/ProductServices/IProductService.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using PriceGetter.Contracts.Products;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PriceGetter.ApplicationServices.ProductServices
 {
     public interface IProductService : IApplicationService
     {
+        Task<IEnumerable<ProductDto>> GetAll();
     }
 }
--- src/PriceGetter.ApplicationService/ProductServices/ProductService.cs
+++ src/PriceGetter.ApplicationService/ProductServices/ProductService.cs
@@ -1,10 +1,51 @@
-﻿using System;
+﻿using PriceGetter.Contracts.Products;
+using PriceGetter.Core.Interfaces.Repositories;
+using PriceGetter.Core.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PriceGetter.ApplicationServices.ProductServices
 {
     public class ProductService : IProductService
     {
+        private readonly IProductRepository productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAll()
+        {
+            IEnumerable<Product> products = await this.productRepository.Get();
+            List<ProductDto> productsDtos = new List<ProductDto>();
+
+            foreach (var product in products)
+            {
+                ProductDto productDto = new ProductDto();
+                productDto.Id = product.Id;
+                productDto.Name = product.Name.ToString();
+
+                List<PriceDto> prices = new List<PriceDto>();
+                foreach (var price in product.PriceHistory)
+                {
+                    PriceDto priceDto = new PriceDto()
+                    {
+                        At = price.At,
+                        Price = price.Amount.Value,
+                        SellerId = price.SellerId
+                    };
+
+                    prices.Add(priceDto);
+                }
+
+                productDto.Prices = prices;
+                productsDtos.Add(productDto);
+            }
+
+            return productsDtos;
+        }
     }
 }

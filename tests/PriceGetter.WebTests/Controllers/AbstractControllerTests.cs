54f8b3d7b00567157482d5215d6c044ab4dd9632
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-08-09 23:24:25 +0200

    Created tests to check if controller method has http method attribute. Declare method in ProductController, not implemented yet

--- tests/PriceGetter.WebTests/Controllers/AbstractControllerTests.cs
+++ tests/PriceGetter.WebTests/Controllers/AbstractControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Routing;
 using PriceGetter.Web.Controllers;
 using System;
 using System.Collections.Generic;
@@ -32,6 +33,54 @@ namespace PriceGetter.WebTests.Controllers
             type.IsPublic.Should().BeTrue();
         }
 
+        [Theory]
+        [MemberData(nameof(GetAllSubclassesOfAbstractController))]
+        public void AllControllersPublicMethods_Should_HaveHttpMethodDeclared(Type type)
+        {
+            type.IsPublic.Should().BeTrue();
+            var methods = type.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public);
+
+        }
+
+        [Theory]
+        [MemberData(nameof(GetAllControllersPublicMethods))]
+        public void EveryPublicMethod_Should_HaveHttpMethodAttribute(MethodInfo method)
+        {
+            bool hasHttpMethodAttribute = method.CustomAttributes
+                .Any(x => x.AttributeType.IsSubclassOf(typeof(HttpMethodAttribute)));
+
+            hasHttpMethodAttribute.Should().BeTrue();
+        }
+
+        [Theory]
+        [MemberData(nameof(GetAllSubclassesOfAbstractController))]
+        public void EveryPublicMethod_Should_HaveHttpMethodAttribute2(Type type)
+        {
+            foreach (var method in type.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public))
+            {
+                bool hasHttpMethodAttribute = method.CustomAttributes
+                .Any(x => x.AttributeType.IsSubclassOf(typeof(HttpMethodAttribute)));
+
+                hasHttpMethodAttribute.Should().BeTrue();
+            }
+        }
+
+        [Fact]
+        public void MethodReturningAllMethods_Should_NotReturnEmptyCollection()
+        {
+            var methods = GetAllControllersPublicMethods();
+
+            methods.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void MethodReturningAllMethods_Should_NotReturnNull()
+        {
+            var methods = GetAllControllersPublicMethods();
+
+            methods.Should().NotBeNull();
+        }
+
         public static IEnumerable<object[]> GetAllNonAbstractPublicClassFromControllersNamespace()
         {
             Assembly assembly = Assembly.GetAssembly(typeof(AbstractController));
@@ -54,5 +103,19 @@ namespace PriceGetter.WebTests.Controllers
 
             return controllers.Select(x => new object[] { x });
         }
+
+        public static IEnumerable<object[]> GetAllControllersPublicMethods()
+        {
+            List<MethodInfo> methods = new List<MethodInfo>();
+            IEnumerable<Type> controllers = GetAllSubclassesOfAbstractController().Select(x => (Type)x.First()).ToList();
+
+            foreach (var type in controllers)
+            {
+                var typeMethods = type.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public);
+                methods.AddRange(typeMethods);
+            }
+
+            return methods.Select(x => new object[] { x });
+        }   
     }
 }

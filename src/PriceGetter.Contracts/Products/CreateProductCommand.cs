795ef70a9d2f95fb34b4f6abae37aad54098f325
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-08-09 22:45:13 +0200

    Added contracts to create product and product follow and 2 controller for those resources

---
+++ src/PriceGetter.Contracts/Products/CreateProductCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceGetter.Contracts.Products
+{
+    public class CreateProductCommand
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/PriceGetter.Contracts/Products/CreateProductFollowCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceGetter.Contracts.Products
+{
+    public class CreateProductFollowCommand
+    {
+        public Guid ProductId { get; set; }
+        public string ProductUrl { get; set; }
+    }
+}

7bf04b09f973c93700cefe085c7ebe46fd612eb1
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-09-27 12:50:47 +0200

    Added prototypes for commands and commands handler

---
+++ src/PriceGetter.ApplicationService/CommandHandlers/ICommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace PriceGetter.ApplicationServices.CommandHandlers
+{
+    public interface ICommandHandler<T> where T : ICommand
+    {
+        void Execute();
+    }
+}

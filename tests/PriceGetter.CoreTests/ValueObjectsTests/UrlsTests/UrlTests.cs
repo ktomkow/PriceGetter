4f7e85731e88ac36662455fb3b458a8be23fb2c1
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-10-10 17:12:21 +0200

    URL FromString tests

---
+++ tests/PriceGetter.CoreTests/ValueObjectsTests/UrlsTests/UrlTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using PriceGetter.Core.Models.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PriceGetter.CoreTests.ValueObjectsTests.UrlsTests
+{
+    public class UrlTests
+    {
+        [Fact]
+        public void FromString_WhenEmptyString_ThenCreateEmptyUrl()
+        {
+            string text = string.Empty;
+
+            Url url = Url.FromString(text);
+
+            url.Should().BeOfType(typeof(EmptyUrl));
+        }
+
+        [Fact]
+        public void FromString_WhenWhitespace_ThenCreateEmptyUrl()
+        {
+            string text = " ";
+
+            Url url = Url.FromString(text);
+
+            url.Should().BeOfType(typeof(EmptyUrl));
+        }
+
+        [Fact]
+        public void FromString_WhenNull_ThenCreateEmptyUrl()
+        {
+            Url url = Url.FromString(null);
+
+            url.Should().BeOfType(typeof(EmptyUrl));
+        }
+
+        [Fact]
+        public void FromString_WhenNonEmptyStringNotStartFromHttpOrHttps_ThenException()
+        {
+            string text = "abcdefghijk";
+
+            Action act = () =>
+            {
+                Url url = Url.FromString(text);
+            };
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Theory]
+        [InlineData("http://dupadupa.pl")]
+        [InlineData("https://dupadupa.pl")]
+        [InlineData("http://jksigwnjgnslkdg")]
+        [InlineData("https://jksigwnjgnslkdg")]
+        public void FromString_WhenNonEmptyStringStartFromHttpOrHttps_ThenCreateUrl(string text)
+        {
+            Url url = Url.FromString(text);
+
+            url.Should().BeOfType(typeof(EmptyUrl));
+        }
+    }
+}

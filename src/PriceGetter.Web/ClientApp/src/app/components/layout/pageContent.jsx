e763be6146702208d7fe3f1d7ae7ab7ccb3ead7f
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-10-14 19:52:45 +0200

    Fixed xs={0} issue

--- src/PriceGetter.Web/ClientApp/src/app/components/layout/pageContent.jsx
+++ src/PriceGetter.Web/ClientApp/src/app/components/layout/pageContent.jsx
@@ -4,11 +4,11 @@ const PageContent = (props) => {
   return (
     <Grid container direction="column">
       <Grid item container xs={12}>
-        <Grid item xs={0} sm={1} />
+        <Grid item xs={false} sm={1} />
         <Grid item container xs={12} sm={10}>
           {props.children}
         </Grid>
-        <Grid item xs={0} sm={1} />
+        <Grid item xs={false} sm={1} />
       </Grid>
     </Grid>
   );

78a2a5885100458dfcd41a44038e80812a9e53ec
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-10-19 22:46:26 +0200

    Removed unused usings

--- src/PriceGetter.Core/Interfaces/IExtractor.cs
+++ src/PriceGetter.Core/Interfaces/IExtractor.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PriceGetter.Core.Interfaces
+﻿namespace PriceGetter.Core.Interfaces
 {
     public interface IExtractor
     {

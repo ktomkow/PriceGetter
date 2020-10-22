26e05290d4c246b2d2d596a3533713e841638036
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-10-23 00:43:56 +0200

    Data provider factory

---
+++ src/PriceGetter.ContentProvider/Factories/DataProviderFactory.cs
@@ -0,0 +1,24 @@
+﻿using PriceGetter.ContentProvider.DataProviders.XKom;
+using PriceGetter.Core.Interfaces.DataProviders;
+using PriceGetter.Core.Interfaces.Factories.DataProviders;
+using PriceGetter.Core.Models.ValueObjects;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PriceGetter.ContentProvider.Factories
+{
+    public class DataProviderFactory : IDataProviderFactory
+    {
+        private readonly IEnumerable<IDataProvider> providers;
+
+        public DataProviderFactory(IEnumerable<IDataProvider> providers)
+        {
+            this.providers = providers;
+        }
+
+        public IDataProvider Create(Url productPage)
+        {
+            return this.providers.Single(x => x is XKomDataProvider);
+        }
+    }
+}
---
+++ src/PriceGetter.Core/Interfaces/Factories/DataProviders/IDataProviderFactory.cs
@@ -0,0 +1,10 @@
+﻿using PriceGetter.Core.Interfaces.DataProviders;
+using PriceGetter.Core.Models.ValueObjects;
+
+namespace PriceGetter.Core.Interfaces.Factories.DataProviders
+{
+    public interface IDataProviderFactory
+    {
+        IDataProvider Create(Url productPage);
+    }
+}

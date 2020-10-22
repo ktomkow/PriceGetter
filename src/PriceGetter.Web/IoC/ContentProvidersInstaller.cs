96a8b97e424cf3d73a537d4b9365990353ad7608
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-10-23 00:31:47 +0200

    Removed register that is not needed any more

--- src/PriceGetter.Web/IoC/ContentProvidersInstaller.cs
+++ src/PriceGetter.Web/IoC/ContentProvidersInstaller.cs
@@ -1,9 +1,5 @@
 ﻿using Autofac;
-using PriceGetter.ApplicationServices.PriceProviders.Sellers;
 using PriceGetter.ContentProvider.DataExtractors;
-using PriceGetter.ContentProvider.DataExtractors.Xkom;
-using PriceGetter.ContentProvider.DataProviders.Xkom;
-using PriceGetter.Core.Interfaces;
 using PriceGetter.Core.Interfaces.DataExtractors;
 
 namespace PriceGetter.Web.IoC
@@ -15,14 +11,6 @@ namespace PriceGetter.Web.IoC
 
             builder.RegisterType<BasicCssExtractor>().As<ICssContentExtractor>().InstancePerLifetimeScope();
             builder.RegisterType<CssPriceExtractor>().As<ICssPriceExtractor>().InstancePerLifetimeScope();
-
-            builder.RegisterType<PriceExtractorXkom>();
-            builder.RegisterType<NameExtractorXkom>();
-            builder.RegisterType<MainImageExtractorXkom>();
-
-            builder.RegisterType<XKomPriceProvider>();
-            builder.RegisterType<XKomNameProvider>();
-            builder.RegisterType<XKomImageProvider>();
         }
     }
 }

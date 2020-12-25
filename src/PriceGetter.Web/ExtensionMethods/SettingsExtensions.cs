c75302715a5a9f1012d0056abc7c707b80de7a02
ktomkow <krzysztof.tomkow@gmail.com>
2020-12-25 18:21:07 +0100

    Settings extensions documentation and local variable name change

--- src/PriceGetter.Web/ExtensionMethods/SettingsExtensions.cs
+++ src/PriceGetter.Web/ExtensionMethods/SettingsExtensions.cs
@@ -1,21 +1,29 @@
 ﻿using Microsoft.Extensions.Configuration;
 using PriceGetter.Infrastructure.Settings;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PriceGetter.Web.ExtensionMethods
 {
+    /// <summary>
+    /// Class containing extension methods to make life easier
+    /// </summary>
     public static class SettingsExtensions
     {
+        /// <summary>
+        /// Method that reads section of appsettings and binds it to a specified type
+        /// </summary>
+        /// <param name="configuration">Object to call method on</param>
+        /// <typeparam name="T">Target type that holds settings from appsettings section. 
+        /// Type name must ends with 'Settings', ex: LoggerSettings type will make the method try to read appsettings section "Logger".</typeparam>
+        /// <returns>Instance of given class</returns>
         public static T GetSettings<T>(this IConfiguration configuration) where T : ISettings, new()
         {
             var section = typeof(T).Name.Replace("Settings", string.Empty);
-            var configurationValue = new T();
-            configuration.GetSection(section).Bind(configurationValue);
 
-            return configurationValue;
+            var settings = new T();
+            
+            configuration.GetSection(section).Bind(settings);
+
+            return settings;
         }
     }
 }

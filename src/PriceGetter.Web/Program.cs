6eb8713cc0b6f1cafc9b030f3677174640e7ac39
ktomkow <krzysztof.tomkow@gmail.com>
2020-12-26 15:52:33 +0100

    Added summary to Program.cs

--- src/PriceGetter.Web/Program.cs
+++ src/PriceGetter.Web/Program.cs
@@ -1,22 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace PriceGetter.Web
 {
+    /// <summary>
+    /// Application starting point
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Application starting point
+        /// </summary>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Application host builder
+        /// </summary>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())

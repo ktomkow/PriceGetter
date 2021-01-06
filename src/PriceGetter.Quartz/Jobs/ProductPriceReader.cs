0c4aa0a97c939f4b7520ac56862bb06bf19a9a34
ktomkow <krzysztof.tomkow@gmail.com>
2021-01-06 19:21:08 +0100

    Added real logic to job that monitors prices

--- src/PriceGetter.Quartz/Jobs/ProductPriceReader.cs
+++ src/PriceGetter.Quartz/Jobs/ProductPriceReader.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Threading.Tasks;
+using PriceGetter.ApplicationServices.Interfaces;
+using PriceGetter.Core.DateTimeAbstraction;
 using PriceGetter.Infrastructure.Logging;
-using Quartz;
 
 namespace PriceGetter.Quartz.Jobs
 {
     public class ProductPriceReader : QuartzSelfRescheduleAction
     {
         private readonly IServiceProvider serviceProvider;
+        private readonly Random random;
 
         public ProductPriceReader(
             IServiceProvider serviceProvider,
@@ -15,6 +17,7 @@ namespace PriceGetter.Quartz.Jobs
             IPriceGetterLogger logger) : base(scheduler, logger)
         {
             this.serviceProvider = serviceProvider;
+            this.random = new Random();
         }
 
         public override async Task Execute()
@@ -30,7 +33,31 @@ namespace PriceGetter.Quartz.Jobs
 
         protected override async Task<DateTime> NextExecutionTime()
         {
-            return await Task.FromResult(DateTime.Now.AddSeconds(5));
+            IPricesWatcher pricesWatcher = this.GetWatcher();
+            if(await pricesWatcher.AnyWorkLeft())
+            {
+                return DateTime.Now.AddSeconds(this.GetRandomSeconds());
+            }
+
+            return DateTimeMethods.TommorowAt(8);
+        }
+
+        private int GetRandomSeconds()
+        {
+            int result = this.random.Next(10, 120);
+            return result;
+        }
+
+        private IPricesWatcher GetWatcher()
+        {
+            IPricesWatcher pricesWatcher = this.serviceProvider.GetService(typeof(IPricesWatcher)) as IPricesWatcher;
+            if (pricesWatcher is null)
+            {
+                this.logger.Error("Cannot resolve IPricesWatcher!");
+                throw new Exception();
+            }
+
+            return pricesWatcher;
         }
     }
 }

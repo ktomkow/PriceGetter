9b080b60271b3d1b4d58b6177b6c27504292f406
ktomkow <krzysztof.tomkow@gmail.com>
2021-01-31 22:05:57 +0100

    Try to run docker container using specified launch profile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM 192.168.0.133:9997/dotnet3p1sdk-node14:latest as build
 WORKDIR /app
 COPY . ./
+RUN dotnet restore
+RUN dotnet build
+RUN dotnet test
 WORKDIR ./src/PriceGetter.Web
 RUN dotnet publish -c Debug -o out
 
 FROM mcr.microsoft.com/dotnet/aspnet:3.1
 COPY --from=build /app/src/PriceGetter.Web/out /app
 WORKDIR /app
-CMD ["dotnet","PriceGetter.Web.dll"]
+ENV LAUNCH_PROFILE='Development'
+CMD ["dotnet","PriceGetter.Web.dll", "--launchProfile", $LAUNCH_PROFILE]
---
+++ src/PriceGetter.Web/Controllers/TechController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using PriceGetter.Infrastructure.Settings;
+
+namespace PriceGetter.Web.Controllers
+{
+    [Route("/tech")]
+    public class TechController : ControllerBase
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly SqlSettings sqlSettings;
+        private readonly LoggerSettings loggerSettings;
+
+        public TechController(IWebHostEnvironment webHostEnvironment, SqlSettings sqlSettings, LoggerSettings loggerSettings)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.sqlSettings = sqlSettings;
+            this.loggerSettings = loggerSettings;
+        }
+
+        public IActionResult Index()
+        {
+            var configuration = new
+            {
+                ApplicationName = this.webHostEnvironment.ApplicationName,
+                HostingEnvironment = this.webHostEnvironment.EnvironmentName,
+                SqlConnectionString = this.sqlSettings.ConnectionString,
+                LoggerPath = this.loggerSettings.LogFilepath
+            };
+
+
+            return Ok(configuration);
+        }
+    }
+}

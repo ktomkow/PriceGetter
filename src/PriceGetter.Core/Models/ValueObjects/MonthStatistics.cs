f09fedff9a98de6bce62ca0d6c4bbd19fa16c7b2
ktomkow <krzysztof.tomkow@gmail.com>
2021-04-18 23:31:16 +0200

    Month statistics tests

--- src/PriceGetter.Core/Models/ValueObjects/MonthStatistics.cs
+++ src/PriceGetter.Core/Models/ValueObjects/MonthStatistics.cs
@@ -12,7 +12,7 @@ namespace PriceGetter.Core.Models.ValueObjects
 
         public int Year { get; }
 
-        public MonthStatistics(Money maxPrice, Money minPrice, int month, int year)
+        public MonthStatistics(Money minPrice, Money maxPrice, int month, int year)
         {
             if (maxPrice >= minPrice)
             {
@@ -21,12 +21,31 @@ namespace PriceGetter.Core.Models.ValueObjects
             }
             else
             {
-                this.MaxPrice = minPrice ?? throw new ArgumentNullException(nameof(maxPrice));
-                this.MinPrice = maxPrice ?? throw new ArgumentNullException(nameof(minPrice));
+                this.MaxPrice = minPrice ?? throw new ArgumentNullException(nameof(minPrice));
+                this.MinPrice = maxPrice ?? throw new ArgumentNullException(nameof(maxPrice));
             }
+
+            this.EnsureMonthIsValid(month);
+            this.EnsureYearIsValid(year);
 
             this.Month = month;
             this.Year = year;
+        }
+
+        private void EnsureMonthIsValid(int month)
+        {
+            if(month > 12 || month < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(month), month, "Month ouf of range 1..12");
+            }
+        }
+
+        private void EnsureYearIsValid(int year)
+        {
+            if (year > 2099 || year < 2000)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year), year, "Month ouf of range 2000..2099");
+            }
         }
     }
 }

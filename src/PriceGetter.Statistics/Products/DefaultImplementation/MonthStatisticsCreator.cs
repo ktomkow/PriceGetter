83203e9be062f5b6b0809a27583523dffcef98ff
ktomkow <krzysztof.tomkow@gmail.com>
2021-04-19 23:13:06 +0200

    First tests for month statistics creator

--- src/PriceGetter.Statistics/Products/DefaultImplementation/MonthStatisticsCreator.cs
+++ src/PriceGetter.Statistics/Products/DefaultImplementation/MonthStatisticsCreator.cs
@@ -1,7 +1,9 @@
 ﻿using PriceGetter.Core.Models.Entities;
 using PriceGetter.Core.Models.ValueObjects;
 using PriceGetter.Statistics.Products.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PriceGetter.Statistics.Products.DefaultImplementation
 {
@@ -9,7 +11,36 @@ namespace PriceGetter.Statistics.Products.DefaultImplementation
     {
         public IEnumerable<MonthStatistics> Create(Product product)
         {
-            throw new System.NotImplementedException();
+            this.EnsureProductNotNull(product);
+
+            if (product.Prices.Any() == false)
+            {
+                return Enumerable.Empty<MonthStatistics>();
+            }
+
+            if (product.Prices.Count() == 1)
+            {
+                Price price = product.GetLastPrice();
+                return new[] { new MonthStatistics(price.Amount, price.Amount, price.At.Month, price.At.Year) };
+            }
+
+            List<MonthStatistics> list = new List<MonthStatistics>();
+
+            Money maxPrice = product.Prices.Max(x => x.Amount);
+            Money minPrice = product.Prices.Min(x => x.Amount);
+            DateTime date = product.Prices.First().At;
+
+            list.Add(new MonthStatistics(minPrice, maxPrice, date.Month, date.Year));
+
+            return list;
+        }
+
+        private void EnsureProductNotNull(Product product)
+        {
+            if(product is null)
+            {
+                throw new ArgumentNullException(nameof(product), this.GetType().FullName);
+            }
         }
     }
 }
---
+++ tests/unit-tests/PriceGetter.Statistics.Tests/MonthStatisticsCreatorTests.cs
@@ -0,0 +1,95 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using PriceGetter.Core.DateTimeAbstraction;
+using PriceGetter.Core.Models.Entities;
+using PriceGetter.Core.Models.ValueObjects;
+using PriceGetter.Statistics.Products.DefaultImplementation;
+using PriceGetter.TestHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace PriceGetter.Statistics.Tests
+{
+    public class MonthStatisticsCreatorTests
+    {
+        private readonly IDateTimeProvider dateTimeProvider;
+        private readonly MonthStatisticsCreator creator;
+
+        public MonthStatisticsCreatorTests()
+        {
+            this.creator = new MonthStatisticsCreator();
+            this.dateTimeProvider = Substitute.For<IDateTimeProvider>();
+        }
+
+        [Fact]
+        public void Create_WhenProductNull_ThenThrowArgumentNullException()
+        {
+            Product product = null;
+
+            Action act = () =>
+            {
+                IEnumerable<MonthStatistics> result = this.creator.Create(product);
+            };
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Create_WhenProductHasNoPrice_ThenReturnEmptyCollection()
+        {
+            Product product = this.GetSampleProduct();
+
+            IEnumerable<MonthStatistics> result = this.creator.Create(product);
+
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Create_WhenOnlyOnePrice_ThenMinAndMaxPriceSame()
+        {
+            Product product = this.GetSampleProduct();
+            product.AddPrice(new Money(19.99m));
+
+            IEnumerable<MonthStatistics> result = this.creator.Create(product);
+
+            result.Should().HaveCount(1);
+            result.First().MaxPrice.Should().Be(result.First().MinPrice).And.Be(new Money(19.99m));
+        }
+
+        [Fact]
+        [ResetDateTimeAbstractions]
+        public void Create_WhenTwoPricesSameMonth_ThenOneObjectButDifferentPrices()
+        {
+            Product product = this.GetSampleProduct();
+            Money firstAmount = new Money(19.99m);
+            Money secondAmount = new Money(1921.21m);
+
+            this.SetDateTime(new DateTime(2019, 2, 23));
+            product.AddPrice(firstAmount);
+
+            this.SetDateTime(new DateTime(2019, 2, 26));
+            product.AddPrice(secondAmount);
+
+            IEnumerable<MonthStatistics> result = this.creator.Create(product);
+
+            result.Should().HaveCount(1);
+            result.First().MinPrice.Should().Be(firstAmount);
+            result.First().MaxPrice.Should().Be(secondAmount);
+            result.First().Year.Should().Be(2019);
+            result.First().Month.Should().Be(2);
+        }
+
+        private Product GetSampleProduct()
+        {
+            return new Product(new Name("SampleName"), new EmptyUrl());
+        }
+
+        private void SetDateTime(DateTime dateTime)
+        {
+            this.dateTimeProvider.UtcNow().Returns(dateTime);
+            DateTimeMethods.OverrideDateTimeProvider(this.dateTimeProvider);
+        }
+    }
+}
